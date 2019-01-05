8786918a41bebce6379c54d57a26e5da7a07a43a
sbesh91 <s.beshensky@gmail.com>
2019-01-05 17:28:47 -0600

    feat: photo grid

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ export const load = async (route) => {
   }
 }
 
-export const generateHeroTransitionAnimation = (toNode, fromNode, toPage, fromPage) => {
+const generateBaseHero = (toNode, fromNode, duration) => {
   const fromRect = fromNode.getBoundingClientRect();
   const toRect = toNode.getBoundingClientRect();
   const deltaLeft = fromRect.left - toRect.left;
@@ -30,35 +30,64 @@ export const generateHeroTransitionAnimation = (toNode, fromNode, toPage, fromPa
   const deltaWidth = fromRect.width / toRect.width;
   const deltaHeight = fromRect.height / toRect.height;
 
-  const duration = 250;
   const animationTimingConfig = {
     fill: 'forwards',
     easing: "cubic-bezier(0.4, 0.0, 0.2, 1)",
-    duration: duration
+    duration: duration,
+    delay: 150
   };
+
+  const clipStart = 'inset(10% 5% 10% 5%)';
+  const clipEnd = 'inset(-6px -6px -6px -6px)';
+
   const frames = [
-    { 'transform': 'translate(' + deltaLeft + 'px,' + deltaTop + 'px) scale(' + deltaWidth + ',' + deltaHeight + ')'},
-    { 'transform': 'none'}
+    { 
+      'transform': 'translate(' + deltaLeft + 'px,' + deltaTop + 'px) scale(' + deltaWidth + ',' + deltaHeight + ')',
+      'opacity': .75,
+      'clipPath': clipStart,
+      '-webkitClipPath': clipStart
+    },
+    { 
+      'transform': 'none',
+      'opacity': 1,
+      'clipPath': clipEnd,
+      '-webkitClipPath': clipEnd
+    }
   ];
+  
+  return { frames, animationTimingConfig };
+}
+
+export const generateHeroTransitionAnimation = (toNode, fromNode, toPage, fromPage) => {
+  const config = generateBaseHero(toNode, fromNode, 250);
   
   toNode.style['transformOrigin'] = '0 0';
   toPage.style.zIndex = 10000;
   toPage.style.visibility = 'visible';
   fromPage.style.visibility = 'hidden';
 
-  // fromPage.animate([
-  //   { 'opacity': 1 },
-  //   { 'opacity': 0 }
-  // ], animationTimingConfig);
-  toNode.animate(frames, animationTimingConfig);
+  toNode.animate(config.frames, config.animationTimingConfig);
+}
+
+export const generateHeroOverlayAnimation = (toNode, fromNode, parentNode) => {
+  const config = generateBaseHero(toNode, fromNode, 350);
+  
+  toNode.style['transformOrigin'] = '0 0';
+  toNode.style.zIndex = 10000;
+  toNode.style.pointerEvents = 'auto';
+  parentNode.style.filter = 'blur(5px)';
+  parentNode.style.opacity = .8;
+  
+
+  toNode.animate(config.frames, config.animationTimingConfig);
 }
 
 export const generatePageTransitionAnimation = (node, direction = 'forwards') => {
-  const duration = 250;
+  const duration = 350;
   const baseFrame = { 'transform': 'none', 'opacity': 1 };
   const modFrame = { 'transform': 'translate(0px,-50px)', 'opacity': 0 };
   const animationTimingConfig = {
-    fill: 'forwards',
+    fill: 'both',
     easing: "cubic-bezier(0.4, 0.0, 0.2, 1)",
     duration: duration
   };
@@ -68,8 +97,9 @@ export const generatePageTransitionAnimation = (node, direction = 'forwards') =>
     frames = [modFrame, baseFrame];
   } else {
     frames = [baseFrame, modFrame]
+    node.style.pointerEvents = 'none';
   }
-
+  
   node.animate(frames, animationTimingConfig);
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,8 @@
 import { LitElement, html } from '@polymer/lit-element/lit-element';
-import * as animateCSSGrid from 'animate-css-grid';
+import { generateHeroOverlayAnimation, generatePageTransitionAnimation } from '../app';
+import { ripple } from '@material/mwc-ripple';
+
+// import * as animateCSSGrid from 'animate-css-grid';
 
 // Create your custom component
 class HomePage extends LitElement {
@@ -7,7 +10,7 @@ class HomePage extends LitElement {
   constructor() {
     super();
     this.images = [];
-    for (let i = 1; i < 10; i++) {
+    for (let i = 1; i < 25; i++) {
       this.images.push(i);
       (async () => {
         await import(`./../photos/${i}.jpg`);
@@ -16,22 +19,28 @@ class HomePage extends LitElement {
   }
 
   firstUpdated() {
-    this.grid = this.shadowRoot.querySelector('#grid');
-    animateCSSGrid.wrapGrid(this.grid, { duration: 300, stagger: 10, easing: 'easeInOut' });
+    // this.grid = this.shadowRoot.querySelector('#grid');
+    // animateCSSGrid.wrapGrid(this.grid, { duration: 300, stagger: 10, easing: 'easeInOut' });
   }
 
-  clickImage(e) {
-    if (e.target.classList.contains('card--expanded')) {
-      e.target.classList.remove('card--expanded');
-    } else {
-      e.target.classList.add('card--expanded');
-    }
+  clickRsvp(e) {
+    generateHeroOverlayAnimation(this.shadowRoot.querySelector('#form'), e.target, this.shadowRoot.querySelector('#grid'));
   }
 
-  getCardClass() {
-    const rand = Math.floor(Math.random() * 4);
-    const classes = ['card--wide', 'card--expanded', 'card--base', 'card--base'];
-    return classes[rand];
+  close(e) {
+    const form = this.shadowRoot.querySelector('#form');
+    const grid = this.shadowRoot.querySelector('#grid');
+    generatePageTransitionAnimation(form, 'backwards');
+    form.style.pointerEvents = 'none';
+    grid.style.opacity = 1;
+    grid.style.filter = 'none';
+  }
+
+  getCardClass(index) {
+    // const rand = Math.floor(Math.random() * 4);
+    const classes = ['card--base', 'card--base', 'card--expanded', 'card--base', 'card--base'];
+    // return classes[rand];
+    return classes[index % 5];
   }
 
   // Define a template
@@ -41,33 +50,66 @@ class HomePage extends LitElement {
       :host {
         width: 100%;
       }
-      div {
-        height: 2rem;
-        width: 2rem;
-        background: black;
-        position: absolute;
-        left: 3rem;
-        top: 10rem;
-      }
       #grid {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-        grid-auto-rows: 12rem;
+        grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+        grid-auto-rows: 1fr;
         grid-gap: .5rem;
         grid-auto-flow: dense;
 
         padding: 1rem;
         min-height: 100vh;
-        background: rgba(0,0,0,.8);
+        transition: 300ms opacity ease;
       }
+      #form {
+        background: white;
+        border-radius: 4px;
+        box-shadow: 0 3px 6px rgba(114,47,55,0.16), 0 3px 6px rgba(114,47,55,0.23);
+        position: fixed;
+        top: calc(50% - 250px);
+        left: calc(50% - 160px);
+        height: 500px;
+        width: 320px;
+        pointer-events: none;
+        opacity: 0;
+      }
+      #rsvp {
+        grid-column: 1 / -1;
+        grid-row: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* position: relative; */
+      }
+      #rsvp div {
+        width: 18rem;
+        height: 8rem;
+        font-size: 5rem;
+        line-height: 5rem;
+        color: #616161;
+        border-radius: 4px;
+        border: none;
+        box-shadow: 0 1px 3px rgba(0,0,0,0.16), 0 1px 3px rgba(0,0,0,0.23);
+        background-image: linear-gradient(90deg, #c3922e 0%, #eed688 40%, #fffbcc 80%, #eed688 100%);
+        pointer-events: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+      }
+
       .card--expanded {
         grid-column: span 2;
         grid-row: span 2;
       }
-      .card--wide {
-        grid-column: span 2;
-        grid-row: span 1;
+
+      @media only screen and (max-width: 600px) {
+        .card--expanded {
+          grid-column: span 1;
+          grid-row: span 1;
+        }
       }
+
       .card--base {
         grid-column: span 1;
         grid-row: span 1;
@@ -77,16 +119,32 @@ class HomePage extends LitElement {
       }
       img {
         pointer-events: none;
+        width: 100%;
       }
     </style>
-    
+
+
     <section id="grid">
+      <div id="rsvp">
+        <div @click="${(e) => this.clickRsvp(e)}">
+          <mwc-ripple></mwc-ripple>
+          RSVP
+        </div>
+      </div>
+
       ${this.images.map(i => html`
-        <main @click="${(e) => this.clickImage(e)}" class="${this.getCardClass(i)}"> 
+        <main class="${this.getCardClass(i)}"> 
           <img src="./src/photos/${i}.jpg">
         </main>
       `)}      
-    </section>`;
+    </section>
+    <div id="form">
+      <span>
+        some form here
+      </span>
+      <button @click="${(e) => this.close(e)}">close</button>
+    </div>
+    `;
   }
 }
 // Register the element with the browser
